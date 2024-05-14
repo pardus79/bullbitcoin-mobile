d968c01a79625acbb948a37274b70c894b1e9bef
saiy2k <saiy2k@gmail.com>
2024-05-14 22:09:52 +0530

    Migration: Fix change address null issue and cleanup

--- lib/_pkg/storage/migration0_1to0_2.dart
+++ lib/_pkg/storage/migration0_1to0_2.dart
@@ -1,6 +1,8 @@
 // Change 1: for each wallet with type as newSeed, change it to secure
-// Change 2: add BaseWalletType as Bitcoin
-// Change 3: create a new Liquid wallet, based on the Bitcoin wallet
+//  Change 2: add BaseWalletType as Bitcoin
+//  Change 3: add isLiquid to all Txns, Addresses
+//  Change 4: Update change address Index
+// Change 5: create a new Liquid wallet, based on the Bitcoin wallet
 import 'dart:convert';
 import 'dart:developer';
 
@@ -17,6 +19,10 @@ import 'package:bb_mobile/_pkg/wallet/lwk/sensitive_create.dart';
 import 'package:bb_mobile/_pkg/wallet/repository/sensitive_storage.dart';
 import 'package:bb_mobile/_pkg/wallet/repository/storage.dart';
 import 'package:bb_mobile/_pkg/wallet/repository/wallets.dart';
+import 'package:bdk_flutter/bdk_flutter.dart' as bdk;
+
+Seed? liquidMainnetSeed;
+Seed? liquidTestnetSeed;
 
 Future<void> doMigration0_1to0_2(
   SecureStorage secureStorage,
@@ -24,10 +30,7 @@ Future<void> doMigration0_1to0_2(
 ) async {
   print('Migration: 0.1 to 0.2');
 
-  // Change 1: for each wallet with type as newSeed, change it to secure
-  // Change 2: add BaseWalletType as Bitcoin
-  final (walletIds, walletIdsErr) =
-      await hiveStorage.getValue(StorageKeys.wallets);
+  final (walletIds, walletIdsErr) = await hiveStorage.getValue(StorageKeys.wallets);
   if (walletIdsErr != null) throw walletIdsErr;
 
   final walletIdsJson = jsonDecode(walletIds!)['wallets'] as List<dynamic>;
@@ -35,83 +38,146 @@ Future<void> doMigration0_1to0_2(
   final WalletSensitiveStorageRepository walletSensitiveStorageRepository =
       WalletSensitiveStorageRepository(secureStorage: secureStorage);
 
-  int mainWalletIndex = 0;
-  int testWalletIndex = 0;
-  Seed? liquidMainnetSeed;
-  Seed? liquidTestnetSeed;
   for (final walletId in walletIdsJson) {
     final (jsn, err) = await hiveStorage.getValue(walletId as String);
     if (err != null) throw err;
 
-    final walletObj = jsonDecode(jsn!) as Map<String, dynamic>;
-    // print(walletObj);
+    Map<String, dynamic> walletObj = jsonDecode(jsn!) as Map<String, dynamic>;
 
-    // TODO: Test this assumption
-    // Assuming first wallet is to be changed to secure and further wallets to words
-    // `newSeed` --> Auto created by wallet
-    // `worlds` --> Wallet recovered by user
-    if (walletObj['type'] == 'newSeed' || walletObj['type'] == 'words') {
-      if (walletObj['network'] == 'Mainnet') {
-        if (mainWalletIndex == 0) {
-          walletObj['type'] = 'secure';
-          walletObj['name'] =
-              'Secure Bitcoin Wallet / ' + (walletObj['name'] as String);
-          walletObj['mainWallet'] = true;
-          mainWalletIndex++;
+    // Change 1: for each wallet with type as newSeed, change it to secure
+    // Change 2: add BaseWalletType as Bitcoin
+    walletObj = await updateWalletObj(walletObj, walletSensitiveStorageRepository);
 
-          final mnemonicFingerprint =
-              walletObj['mnemonicFingerprint'] as String;
-          final (seed, _) = await walletSensitiveStorageRepository.readSeed(
-            fingerprintIndex: mnemonicFingerprint,
-          );
+    // Change 3: add isLiquid to all Txns, Addresses
+    walletObj = await addIsLiquid(walletObj);
 
-          liquidMainnetSeed = seed;
-        } else {
-          walletObj['type'] = 'words';
-          mainWalletIndex++;
-        }
-      } else if (walletObj['network'] == 'Testnet') {
-        if (testWalletIndex == 0) {
-          walletObj['type'] = 'secure';
-          walletObj['name'] =
-              'Secure Bitcoin Wallet / ' + (walletObj['name'] as String);
-          walletObj['mainWallet'] = true;
-          testWalletIndex++;
-
-          final mnemonicFingerprint =
-              walletObj['mnemonicFingerprint'] as String;
-          final (seed, _) = await walletSensitiveStorageRepository.readSeed(
-            fingerprintIndex: mnemonicFingerprint,
-          );
-
-          liquidTestnetSeed = seed;
-        } else {
-          walletObj['type'] = 'words';
-          testWalletIndex++;
-        }
-      }
-    }
-    walletObj.addAll({'baseWalletType': 'Bitcoin'});
+    // Change 4: Update change address Index
+    walletObj = await updateChangeAddressIndex(walletObj);
 
     // print('Save wallet as:');
     // print(jsonEncode(walletObj));
 
-    final _ = await hiveStorage.saveValue(
-      key: walletId,
-      value: jsonEncode(
-        walletObj,
-      ),
-    );
+    // final _ = await hiveStorage.saveValue(
+    //   key: walletId,
+    //   value: jsonEncode(
+    //     walletObj,
+    //   ),
+    // );
   }
-
-  // Change 3: add isLiquid to all Txns, Addresses
-  await addIsLiquid(secureStorage, hiveStorage);
 
   // Change 4: create a new Liquid wallet, based on the Bitcoin wallet
   await createLiquidWallet(liquidMainnetSeed, liquidTestnetSeed, hiveStorage);
 
   // Finally update version number to next version
   await secureStorage.saveValue(key: StorageKeys.version, value: '0.2');
+}
+
+Future<Map<String, dynamic>> updateWalletObj(
+  Map<String, dynamic> walletObj,
+  WalletSensitiveStorageRepository walletSensitiveStorageRepository,
+) async {
+  int mainWalletIndex = 0;
+  int testWalletIndex = 0;
+
+  // TODO: Test this assumption
+  // Assuming first wallet is to be changed to secure and further wallets to words
+  // `newSeed` --> Auto created by wallet
+  // `worlds` --> Wallet recovered by user
+  if (walletObj['type'] == 'newSeed' || walletObj['type'] == 'words') {
+    if (walletObj['network'] == 'Mainnet') {
+      if (mainWalletIndex == 0) {
+        walletObj['type'] = 'secure';
+        walletObj['name'] = 'Secure Bitcoin Wallet / ' + (walletObj['name'] as String);
+        walletObj['mainWallet'] = true;
+        mainWalletIndex++;
+
+        final mnemonicFingerprint = walletObj['mnemonicFingerprint'] as String;
+        final (seed, _) = await walletSensitiveStorageRepository.readSeed(
+          fingerprintIndex: mnemonicFingerprint,
+        );
+
+        liquidMainnetSeed = seed;
+      } else {
+        walletObj['type'] = 'words';
+        mainWalletIndex++;
+      }
+    } else if (walletObj['network'] == 'Testnet') {
+      if (testWalletIndex == 0) {
+        walletObj['type'] = 'secure';
+        walletObj['name'] = 'Secure Bitcoin Wallet / ' + (walletObj['name'] as String);
+        walletObj['mainWallet'] = true;
+        testWalletIndex++;
+
+        final mnemonicFingerprint = walletObj['mnemonicFingerprint'] as String;
+        final (seed, _) = await walletSensitiveStorageRepository.readSeed(
+          fingerprintIndex: mnemonicFingerprint,
+        );
+
+        liquidTestnetSeed = seed;
+      } else {
+        walletObj['type'] = 'words';
+        testWalletIndex++;
+      }
+    }
+  }
+  walletObj.addAll({'baseWalletType': 'Bitcoin'});
+  return walletObj;
+}
+
+Future<Map<String, dynamic>> updateChangeAddressIndex(
+  Map<String, dynamic> walletObj,
+) async {
+  int changeAddressCount = 0;
+  if (walletObj['myAddressBook'] != null) {
+    walletObj['myAddressBook'] =
+        walletObj['myAddressBook'].map((addr) => addr as Map<String, dynamic>).map((addr) {
+      if (addr['kind'] == 'change') {
+        changeAddressCount++;
+      }
+      return addr;
+    }).toList();
+  }
+
+  final Wallet w = Wallet.fromJson(walletObj);
+  final WalletsRepository walletRepo = WalletsRepository();
+  final bdkCreate = BDKCreate(walletsRepository: walletRepo);
+  final (bdkWallet, _) = await bdkCreate.loadPublicBdkWallet(w);
+
+  for (int i = 0; i < changeAddressCount; i++) {
+    final nativeAddr = await bdkWallet!.getAddress(addressIndex: bdk.AddressIndex.peek(index: i));
+    final nativeAddrStr = await nativeAddr.address.asString();
+    final matchIndex = w.myAddressBook.indexWhere((a) => a.address == nativeAddrStr);
+    w.myAddressBook[matchIndex] = w.myAddressBook[matchIndex].copyWith(index: nativeAddr.index);
+  }
+
+  return w.toJson();
+}
+
+Future<Map<String, dynamic>> addIsLiquid(
+  Map<String, dynamic> walletObj,
+) async {
+  walletObj['transactions'] = walletObj['transactions']
+      .map((tx) => tx as Map<String, dynamic>)
+      .map((tx) => tx..addAll({'isLiquid': false}))
+      .toList();
+
+  if (walletObj['myAddressBook'] != null) {
+    walletObj['myAddressBook'] = walletObj['myAddressBook']
+        .map((addr) => addr as Map<String, dynamic>)
+        .map((addr) => addr..addAll({'isLiquid': false}))
+        .toList();
+  }
+
+  log(jsonEncode(walletObj['myAddressBook']));
+
+  if (walletObj['externalAddressBook'] != null) {
+    walletObj['externalAddressBook'] = walletObj['externalAddressBook']
+        .map((addr) => addr as Map<String, dynamic>)
+        .map((addr) => addr..addAll({'isLiquid': false}))
+        .toList();
+  }
+
+  return walletObj;
 }
 
 Future<void> createLiquidWallet(
@@ -148,8 +214,7 @@ Future<void> createLiquidWallet(
       network: BBNetwork.Mainnet,
       walletCreate: walletCreate,
     );
-    final liquidWallet =
-        lw?.copyWith(name: lw.creationName(), mainWallet: true);
+    final liquidWallet = lw?.copyWith(name: lw.creationName(), mainWallet: true);
     // print(liquidWallet?.id);
     await walletsStorageRepository.newWallet(liquidWallet!);
   }
@@ -165,61 +230,8 @@ Future<void> createLiquidWallet(
       network: BBNetwork.Testnet,
       walletCreate: walletCreate,
     );
-    final liquidWallet =
-        lw?.copyWith(name: lw.creationName(), mainWallet: true);
+    final liquidWallet = lw?.copyWith(name: lw.creationName(), mainWallet: true);
     // print(liquidWallet?.id);
     await walletsStorageRepository.newWallet(liquidWallet!);
-  }
-}
-
-Future<void> addIsLiquid(
-  SecureStorage secureStorage,
-  HiveStorage hiveStorage,
-) async {
-  final (walletIds, walletIdsErr) =
-      await hiveStorage.getValue(StorageKeys.wallets);
-  if (walletIdsErr != null) throw walletIdsErr;
-
-  final walletIdsJson = jsonDecode(walletIds!)['wallets'] as List<dynamic>;
-
-  final WalletSensitiveStorageRepository walletSensitiveStorageRepository =
-      WalletSensitiveStorageRepository(secureStorage: secureStorage);
-
-  for (final walletId in walletIdsJson) {
-    final (jsn, err) = await hiveStorage.getValue(walletId as String);
-    if (err != null) throw err;
-
-    final walletObj = jsonDecode(jsn!) as Map<String, dynamic>;
-    walletObj['transactions'] = walletObj['transactions']
-        .map((tx) => tx as Map<String, dynamic>)
-        .map((tx) => tx..addAll({'isLiquid': false}))
-        .toList();
-    //log(jsonEncode(walletObj['transactions']));
-
-    // log(jsonEncode(walletObj['myAddressBook']));
-    if (walletObj['myAddressBook'] != null) {
-      walletObj['myAddressBook'] = walletObj['myAddressBook'].map(
-        (addr) {
-          addr.addAll({'isLiquid': false});
-          return addr;
-        },
-      ).toList();
-    }
-
-    if (walletObj['myAddressBook'] != null) {
-      walletObj['externalAddressBook'] = walletObj['externalAddressBook']
-          .map((addr) => addr as Map<String, dynamic>)
-          .map((addr) => addr..addAll({'isLiquid': false}))
-          .toList();
-    }
-
-    log(jsonEncode(walletObj['myAddressBook']));
-
-    // final _ = await hiveStorage.saveValue(
-    //   key: walletId,
-    //   value: jsonEncode(
-    //     walletObj,
-    //   ),
-    // );
   }
 }
